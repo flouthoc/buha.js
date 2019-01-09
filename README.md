c82c1b989bc5293c77b3e0e4222542ae4a7141bf
flouthoc <flouthoc.git@gmail.com>
2019-01-09 08:40:19 +0530

    first

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # buha.js
+Browser based <b>Strictly ordered</b> Task Queue for <b>Sync/Async</b> Javascript Functions
+
+## Usage
+
+Both <b>Sync/Async</b> Jobs can be submitted to buhaRunner with simple push function
+
+```javascript
+const buhaRunner = buha();
+buhaRunner.push(job1);
+buhaRunner.push(job2);
+```
+
+Async Function with Callback example
+```javascript
+const exampletask = async (callback) => {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done!"), 1000)
+  });
+
+  let result = await promise; // wait till the promise resolves (*)
+
+  alert(result); // "done!"
+  callback();
+}
+
+const buhaRunner = buha();
+buhaRunner(exampletask);
+```
+<b>Sync</b> Function with Callback as argument to push not to function itself.
+```js
+const buhaRunner = buha();
+buhaRunner(()=>{console.log("hello"), ()=>{console.log("Quick Callback"}, true);
+```
+
+## Docs
+
+### .push(job, callback, isSync)
+
+* Job : main job function
+* Callback: callback if not specifed in function itself
+* isSync: must be true if function is Synchronus.
+
+### Contributors
+```add your name here```
+
+### Leagal
+Any Images or artwork used in this project are part of freepik.
+
+
---
+++ buha.js
@@ -0,0 +1,108 @@
+function buha(callback){
+
+	var mutex = false;
+	const buhaqueue = [];
+
+	const buhaPush = (task, taskcallback, isSync) => {
+
+		if(typeof taskcallback === 'function' || isSync){
+
+			buhaqueue.push({task: task, taskcallback: taskcallback, isSync: isSync});
+		}
+		else if(typeof task === 'function'){
+
+			buhaqueue.push(task);
+		}
+
+	}
+
+
+	const buhaRun = (task, taskcallback, isSync) => {
+
+		mutex = true;
+	
+		try{
+
+			task((error) => {
+			
+				if(error){
+
+					console.error("Buha.js first : Error in executing task, stopping queue");
+					console.error("Buha.js : "+error);
+
+					if(typeof callback === 'function'){
+						callback(err);
+					}
+
+				} else {
+
+					mutex = false;
+					//alert('mutex false');
+					if(buhaqueue.length > 0){
+						//alert(buhaqueue.length);
+						
+						let taskobject = buhaqueue.shift();
+						if(typeof taskobject === 'function'){
+							buhaRun(taskobject);
+						}else{
+
+							if(typeof taskobject.taskcallback === 'function'){
+				
+								buhaRun(taskobject.task, taskobject.taskcallback, taskobject.isSync);
+							}else{
+								
+								buhaRun(taskobject.task, null, taskobject.isSync);
+							}
+						}
+					}
+				}	
+
+			});
+
+					
+
+			
+			if(isSync == true){
+				//alert('do');
+
+				mutex = false;
+
+				let taskobject = buhaqueue.shift();
+				if(taskobject){
+					if(typeof taskobject === 'function'){
+							buhaRun(taskobject);
+					}else{
+
+						if(typeof taskobject.taskcallback === 'function'){
+							buhaRun(taskobject.task, taskobject.taskcallback, taskobject.isSync);
+						}else{
+							buhaRun(taskobject.task, null, taskobject.isSync);
+						}
+					}
+				}
+			}
+
+		}catch (error){
+
+
+			console.error("Buha.js : Error in executing task, stopping queue");
+			console.error("Buha.js : "+error);
+
+			if(typeof callback === 'function'){
+					callback(err);
+			}
+
+			if(taskcallback){
+				if(typeof taskcallback === 'function'){
+							taskcallback(error);
+				}
+			}
+		}
+	}
+
+
+	return{
+
+		push: (task , taskcallback, isSync) => { return mutex ? buhaPush(task, taskcallback, isSync) : buhaRun(task, taskcallback, isSync) },
+	}
+}
